c76654fc70073222e907a23c34dc296d9c26a812
mos20 <mos20@6bc7dc0d-0d55-4223-bbc0-b7e8fef45973>
2009-08-02 18:12:56 +0000

    Implement the parser side of staj.

---
+++ argo-vcs/src/main/java/argo/staj/ParserToJsonStreamReaderAdapter.java
@@ -0,0 +1,60 @@
+package argo.staj;
+
+import argo.token.JsonListenerException;
+import argo.token.JsonParser;
+
+import java.io.IOException;
+import java.io.Reader;
+
+public final class ParserToJsonStreamReaderAdapter implements JsonStreamReader {
+    
+    private final BlockingJsonListener blockingJsonListener;
+    private Element next;
+
+    public ParserToJsonStreamReaderAdapter(final Reader in) throws JsonListenerException, IOException {
+        blockingJsonListener = new BlockingJsonListener();
+        new Thread("Argo parser") {
+            @Override
+            public void run() {
+                try {
+                    new JsonParser().parse(in, blockingJsonListener);
+                } catch (JsonListenerException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                } catch (IOException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                } finally {
+                    blockingJsonListener.close();
+                }
+            }
+        }.start();
+    }
+
+    public JsonStreamElementType next() throws JsonStreamException {
+        next = blockingJsonListener.getNext();
+        return next.getJsonStreamElementType();
+    }
+
+    public String getElementText() throws JsonStreamException {
+        return next.getText();
+    }
+
+    public boolean hasNext() throws JsonStreamException {
+        return JsonStreamElementType.END_DOCUMENT == next.getJsonStreamElementType();
+    }
+
+    public void close() throws JsonStreamException {
+        blockingJsonListener.close();
+    }
+
+    public JsonStreamElementType getEventType() {
+        return next.getJsonStreamElementType();
+    }
+
+    public String getText() {
+        return next.getText();
+    }
+
+    public boolean hasText() {
+        return next.hasText();
+    }
+}
---
+++ argo-vcs/src/test/java/argo/staj/ParserToJsonStreamReaderAdapterTest.java
@@ -0,0 +1,7 @@
+package argo.staj;
+
+public final class ParserToJsonStreamReaderAdapterTest {
+
+    
+
+}
