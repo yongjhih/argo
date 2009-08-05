358b83169c2dde1e4b92b5468db4f4e797aa9576
mos20 <mos20@6bc7dc0d-0d55-4223-bbc0-b7e8fef45973>
2009-08-05 19:21:02 +0000

    Make JsonListenerException package local.

--- argo-vcs/src/main/java/argo/xml/JsonListenerException.java
+++ argo-vcs/src/main/java/argo/xml/JsonListenerException.java
@@ -1,19 +1,19 @@
 package argo.xml;
 
-public final class JsonListenerException extends RuntimeException {
-    public JsonListenerException() {
+final class JsonListenerException extends RuntimeException {
+    JsonListenerException() {
         super();
     }
 
-    public JsonListenerException(final String message) {
+    JsonListenerException(final String message) {
         super(message);
     }
 
-    public JsonListenerException(final String message, final Throwable cause) {
+    JsonListenerException(final String message, final Throwable cause) {
         super(message, cause);
     }
 
-    public JsonListenerException(final Throwable cause) {
+    JsonListenerException(final Throwable cause) {
         super(cause);
     }
 }
