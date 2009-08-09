990c3706bbe060dd8b4bb161c672cfb7606c8425
mos20 <mos20@6bc7dc0d-0d55-4223-bbc0-b7e8fef45973>
2009-08-09 22:05:08 +0000

    Update documentation a bit.

---
+++ docs/piwik_1.js
@@ -0,0 +1,2 @@
+var pkBaseURL = (("https:" == document.location.protocol) ? "https://sourceforge.net/apps/piwik/argo/" : "http://sourceforge.net/apps/piwik/argo/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
---
+++ docs/piwik_2.js
@@ -0,0 +1,4 @@
+piwik_action_name = '';
+piwik_idsite = 1;
+piwik_url = pkBaseURL + "piwik.php";
+piwik_log(piwik_action_name, piwik_idsite, piwik_url);
