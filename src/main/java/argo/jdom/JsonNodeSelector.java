c768df109c0dc7c8806febe6fc221d6c458ecb90
mos20 <mos20@6bc7dc0d-0d55-4223-bbc0-b7e8fef45973>
2009-08-25 17:55:46 +0000

    More javadoc.

--- src/main/java/argo/jdom/JsonNodeSelector.java
+++ src/main/java/argo/jdom/JsonNodeSelector.java
@@ -32,10 +32,22 @@ public final class JsonNodeSelector<T, U> {
         this.valueGetter = valueGetter;
     }
 
+    /**
+     * Determines whether this <code>JsonNodeSelector</code> can exctract a value from the given <code>JsonNode</code>.
+     * @param jsonNode the <code>JsonNode</code> to test.
+     * @return true if a value can be extracted from the given <code>JsonNode</code>, false otherwise. 
+     */
     public boolean matches(final T jsonNode) {
         return valueGetter.matchesNode(jsonNode);
     }
 
+    /**
+     * Extracts a value from the give <code>JsonNode</code>.
+     *
+     * @param argument the <code>JsonNode</code> to extract a value from.
+     * @return the extracted value.
+     * @throws IllegalArgumentException if calling <code>matches<code> with the given <code>JsonNode</code> would return false, indicating no value can be extracted from it. 
+     */
     public U getValue(final T argument) {
         if (!matches(argument)) {
             throw new IllegalArgumentException("Argument [" + argument + "] does not match the requirments of this selector.");
@@ -44,6 +56,18 @@ public final class JsonNodeSelector<T, U> {
         }
     }
 
+    /**
+     * <p>Constructs a JsonNodeSelector consisting of this chained with the given <code>JsonNodeSelector</code>.</p>
+     *
+     * <p>For example, if we have <code>JsonNodeSelectors</code> for the first element of an array, and another that
+     * selects the second element of an array, and we chain them together in that order, we will get a selector that
+     * works on nested arrays, selecting the second element from an array stored in the first element of a parent
+     * array.</p> 
+     *
+     * @param childJsonNodeSelector the <code>JsonNodeSelector</code> to chain onto this.
+     * @param <V> the type the chained <code>JsonNodeSelector</code> will return.
+     * @return a new <code>JsonNodeSelector</code> representing this, and the given selector applied in sequence.
+     */
     public <V> JsonNodeSelector<T, V> with(final JsonNodeSelector<U, V> childJsonNodeSelector) {
         return new JsonNodeSelector<T,V>(new ChainedFunctor<T, U, V>(this, childJsonNodeSelector));
     }
