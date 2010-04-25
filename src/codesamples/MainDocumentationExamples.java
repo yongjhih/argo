cd8248e54021a909f9c714d0ef4fbcb9af1737dc
mos20 <mos20@6bc7dc0d-0d55-4223-bbc0-b7e8fef45973>
2010-04-25 08:10:35 +0000

    Remove an unused interface.

--- src/codesamples/MainDocumentationExamples.java
+++ src/codesamples/MainDocumentationExamples.java
@@ -125,7 +125,7 @@ public final class MainDocumentationExamples {
     public void producesJsonBackedObject() throws Exception {
         final String jsonText = FileUtils.readFileToString(new File(this.getClass().getResource("SimpleExample.json").getFile()));
         final JsonRootNode json = JDOM_PARSER.parse(jsonText);
-        final SimpleExample simpleExample = new JsonBackedSimpleExample(json);
+        final SimpleExample simpleExample = new SimpleExample(json);
         System.out.println("simpleExample.getName() = " + simpleExample.getName());
         System.out.println("simpleExample.getSales() = " + simpleExample.getSales());
         System.out.println("simpleExample.getSingles() = " + simpleExample.getSingles());
--- src/codesamples/SimpleExample.java
+++ src/codesamples/SimpleExample.java
@@ -8,11 +8,45 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
+import argo.jdom.JsonNode;
+import argo.jdom.JsonNodeSelector;
+import argo.jdom.JsonRootNode;
+
+import java.util.AbstractList;
 import java.util.List;
 
-public interface SimpleExample {
+import static argo.jdom.JsonNodeSelectors.*;
 
-    String getName();
-    int getSales();
-    List<String> getSingles();
+public final class SimpleExample {
+
+    private static final JsonNodeSelector<JsonNode, String> NAME_SELECTOR = aStringNode("name");
+    private static final JsonNodeSelector<JsonNode, String> SALES_SELECTOR = aNumberNode("sales");
+    private static final JsonNodeSelector<JsonNode, List<JsonNode>> SINGLES_SELECTOR = anArrayNode("singles");
+    private static final JsonNodeSelector<JsonNode, String> SINGLE_NAME_SELECTOR = aStringNode();
+
+    private final JsonRootNode json;
+
+    public SimpleExample(final JsonRootNode json) {
+        this.json = json;
+    }
+
+    public String getName() {
+        return NAME_SELECTOR.getValue(json);
+    }
+
+    public int getSales() {
+        return Integer.valueOf(SALES_SELECTOR.getValue(json));
+    }
+
+    public List<String> getSingles() {
+        return new AbstractList<String>() {
+            public String get(final int index) {
+                return SINGLE_NAME_SELECTOR.getValue(SINGLES_SELECTOR.getValue(json).get(index));
+            }
+
+            public int size() {
+                return SINGLES_SELECTOR.getValue(json).size();
+            }
+        };
+    }
 }
