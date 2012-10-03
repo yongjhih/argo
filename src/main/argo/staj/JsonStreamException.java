23ce5077daee95db6377e2be758115a09f296237
mos20 <mos20@6bc7dc0d-0d55-4223-bbc0-b7e8fef45973>
2012-10-03 20:49:54 +0000

    Delete some unused methods.

--- src/main/argo/staj/JsonStreamException.java
+++ src/main/argo/staj/JsonStreamException.java
@@ -14,19 +14,9 @@ package argo.staj;
  * Thrown to indicate that it was not possible to read any further along the JSON stream.
  */
 public final class JsonStreamException extends RuntimeException {
-    public JsonStreamException() {
-        super();
-    }
 
-    public JsonStreamException(final String message) {
-        super(message);
-    }
-
-    public JsonStreamException(final String message, final Throwable cause) {
+    JsonStreamException(final String message, final Throwable cause) {
         super(message, cause);
     }
 
-    public JsonStreamException(final Throwable cause) {
-        super(cause);
-    }
 }
