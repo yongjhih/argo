251e5a4e0cb4b5033c8ed8258192f0de6ad547cd
mos20 <mos20@6bc7dc0d-0d55-4223-bbc0-b7e8fef45973>
2014-07-02 22:19:59 +0000

    Mark - JSON escape Strings in a single pass.

--- src/main/argo/format/JsonEscapedString.java
+++ src/main/argo/format/JsonEscapedString.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Mark Slater
+ * Copyright 2014 Mark Slater
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  *
@@ -10,20 +10,41 @@
 
 package argo.format;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 final class JsonEscapedString {
+
+    private static final Pattern ESCAPE_CHARACTERS_PATTERN = Pattern.compile("(\\\\|\\\"|\b|\\f|\\n|\\r|\\t)");
 
     private JsonEscapedString() {
     }
 
-    static String escapeString(String unescapedString) {
-        return unescapedString
-                .replace("\\", "\\\\")
-                .replace("\"", "\\\"")
-                .replace("\b", "\\b")
-                .replace("\f", "\\f")
-                .replace("\n", "\\n")
-                .replace("\r", "\\r")
-                .replace("\t", "\\t");
+    static String escapeString(final String unescapedString) {
+        final Matcher matcher = ESCAPE_CHARACTERS_PATTERN.matcher(unescapedString);
+        final StringBuffer sb = new StringBuffer();
+        while (matcher.find()) {
+            final String match = matcher.group();
+            if ("\\".equals(match)) {
+                matcher.appendReplacement(sb, "\\\\\\\\");
+            } else if ("\"".equals(match)) {
+                matcher.appendReplacement(sb, "\\\\\\\"");
+            } else if ("\b".equals(match)) {
+                matcher.appendReplacement(sb, "\\\\b");
+            } else if ("\f".equals(match)) {
+                matcher.appendReplacement(sb, "\\\\f");
+            } else if ("\n".equals(match)) {
+                matcher.appendReplacement(sb, "\\\\n");
+            } else if ("\r".equals(match)) {
+                matcher.appendReplacement(sb, "\\\\r");
+            } else if ("\t".equals(match)) {
+                matcher.appendReplacement(sb, "\\\\t");
+            } else {
+                throw new RuntimeException("Coding failure in Argo: JSON String escaping matched a character [" + match + "] for which no replacement is defined.");
+            }
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
     }
 
 }
